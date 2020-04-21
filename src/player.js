d97b042819c1249b1a03871e2ac566c58af4b42f
Suzie Q <suzieqwolf@gmail.com>
2020-04-20 20:12:57 -0600

    My second run, not near close to done, Have a lot of editing to do and need to get save to storage functioning.

--- src/player.js
+++ src/player.js
@@ -9,23 +9,23 @@ class Player {
         this.gameHistory = [];
     }
 
-    saveUserInStorage() {
-        var userToStore = {id: this.id, name: this.name, image: this.image, gameHistory: this.gameHistory}
-        var stringifiedUser = JSON.stringify(userToStore)
-        localStorage.setItem(`storedIdea${this.id}`, stringifiedUser) 
-    }   
-
-    getUserGameRecord() {
-        retrieveFromStorage()
-        displayRecord()
-        displayWinBoards()
-    }
-
-    
-    updateActionStatus() {
-        this.actionRequired = true !== false ? false:true;
-    }
-
+   
     //id == user input
     //loop to check if id has been used previously, if not, then ok, else 'pick new name'
+} saveUserInStorage() {
+    var userToStore = {id: this.id, name: this.name, image: this.image, gameHistory: this.gameHistory}
+    var stringifiedUser = JSON.stringify(userToStore)
+    localStorage.setItem(`storedIdea${this.id}`, stringifiedUser) 
+}   
+
+getUserGameRecord() {
+    retrieveFromStorage()
+    displayRecord()
+    displayWinBoards()
 }
+
+
+updateActionStatus() {
+    this.actionRequired = true !== false ? false:true;
+}
+
